8a76806201f59957b7c2546131b87a50d7acb391
codepot001 <60437210+codepot001@users.noreply.github.com>
2022-12-29 11:04:19 +0900

    init project

---
+++ src/test/java/me/study/ecommercebackend/EcommercebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.study.ecommercebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
